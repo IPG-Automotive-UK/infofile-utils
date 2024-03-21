cb197d993d1a5e6de0d60428581880023b448588
arpadlomenipg <arpad.lomen@ipg-automotive.com>
2024-03-21 17:51:35 +0100

    validations to Vehicle ands TestRun infofiles

---
+++ src/isInfoFile.js
@@ -0,0 +1,73 @@
+const { readInfoFile } = require("./utils");
+
+// helper function to determine the validity of the provided infofile type
+function isValidType(type) {
+  switch (type) {
+    case "Vehicle":
+      return true;
+    case "TestRun":
+      return true;
+    case "Model":
+      return true;
+    default:
+      return false;
+  }
+}
+
+// a function to determine whether a file is an infofile or not
+function isInfoFile({ file, type }) {
+  // check that the file has been provided
+  if (!file) {
+    throw new Error("file is required");
+  }
+
+  // check that type has been provided
+  if (!type) {
+    throw new Error("type is required");
+  }
+
+  if (!isValidType(type)) {
+    throw new Error("type is invalid");
+  }
+
+  // read the info file
+  const infofile = readInfoFile(file);
+  const fileIdent = infofile.getString("FileIdent");
+
+  // looks for the appropriate condition to validate the infofile based on the provided type
+  switch (type) {
+    // regex match for possible vehicle types in the FileIdent property of the infofile
+    case "Vehicle":
+      if (
+        fileIdent.match(/^CarMaker-Car\s\d{2}$/) ||
+        fileIdent.match(/^CarMaker-Truck\s\d{2}$/) ||
+        fileIdent.match(/^CarMaker-Motorcycle\s\d{2}$/)
+      ) {
+        // delete the infofile handle
+        infofile.delete();
+        return true;
+      } else {
+        // delete the infofile handle
+        infofile.delete();
+        return false;
+      }
+    // regex match for TestRun in the FileIdent property of the infofile
+    case "TestRun":
+      if (fileIdent.match(/^CarMaker-TestRun\s\d{2}$/)) {
+        // delete the infofile handle
+        infofile.delete();
+        return true;
+      } else {
+        // delete the infofile handle
+        infofile.delete();
+        return false;
+      }
+    default:
+      // delete the infofile handle
+      infofile.delete();
+      return false;
+  }
+}
+
+// export is infofile function
+module.exports = { isInfoFile };
